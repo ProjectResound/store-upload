99d9070f5dc17a92385fb36ea373193319a28632
Louise Yang <louise.m.yang@gmail.com>
2017-05-25 13:11:28 -0700

    search spec

--- test/search.spec.js
+++ test/search.spec.js
@@ -1,6 +1,27 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import TestUtils from 'react-dom/test-utils'
-//
-// const expect = require('chai').expect;
-// const Search = require('../src/scripts/components/search/Search');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-dom/test-utils'
+import expect from 'chai';
+import Search from '../src/scripts/components/search/Search';
+
+describe('<Search />', function() {
+  beforeEach(() => {
+    this.component = TestUtils.renderIntoDocument(<Search />);
+    this.searchDOM = ReactDOM.findDOMNode(this.component);
+  });
+
+  afterEach(() => {
+    ReactDOM.unmountComponentAtNode(this.searchDOM.parentNode);
+  });
+
+  it('renders a search component');
+
+  context('user tapping on search button', () => {
+    it('calls resoundAPI if there is a query');
+    it('does nothing if there is no query');
+  });
+
+  context('when resoundAPI returns results', () => {
+
+  });
+});
