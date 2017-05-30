55b4757c80880074313dedf345fef5d93b647201
arellaTV <jeremiah.arella@gmail.com>
2017-05-30 00:38:44 -0700

    Enables append test

--- test/explorer.spec.js
+++ test/explorer.spec.js
@@ -71,7 +71,7 @@ describe('<Explorer />', function () {
     expect(eachColumnIsRendered).to.be.true;
   });
 
-  xit('adds a single file after a successful upload', () => {
+  it('adds a single file after a successful upload', () => {
     const listBeforeUpload = ExplorerStore.getAudioList();
     const actionStub = { response: _respondWithFiles() };
     const listAfterUpload = ExplorerStore.appendAudioList(actionStub);
