e2f96f8c6e9a97fab2d8e6dee7dfe898b5416c3f
Louise Yang <louise.m.yang@gmail.com>
2017-07-25 13:02:36 -0700

    Add docker changes for custom nginx.conf for production

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,4 +4,4 @@ sed -i "s#MAGICSTRING_AUTH0_CLIENT_ID#$AUTH0_CLIENT_ID#g" /usr/share/nginx/html/
 sed -i "s#MAGICSTRING_AUTH0_CALLBACK_URL#$AUTH0_CALLBACK_URL#g" /usr/share/nginx/html/scripts/bundle.js
 sed -i "s#MAGICSTRING_AUTH0_AUDIENCE#$AUTH0_AUDIENCE#g" /usr/share/nginx/html/scripts/bundle.js
 
-nginx -g 'daemon off;'
+nginx -g 'daemon off;' -c /usr/share/nginx/nginx.conf -p /usr/share/nginx/html
