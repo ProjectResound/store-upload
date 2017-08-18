97cfb1747703e9b5e0c4fe24307ba3a70452ca97
Louise Yang <louise.m.yang@gmail.com>
2017-08-18 10:50:33 -0700

    Adds a top z-index for the modal

--- test/header.spec.js
+++ test/header.spec.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { MemoryRouter } from 'react-router-dom';
-import TestUtils from 'react-dom/test-utils'
-import {expect, assert} from 'chai';
-import sinon from 'sinon';
-import auth0 from 'auth0-js';
-import Header from '../src/scripts/components/Header';
-import resoundAPI from '../src/scripts/services/resound-api';
+import React from "react";
+import ReactDOM from "react-dom";
+import { MemoryRouter } from "react-router-dom";
+import TestUtils from "react-dom/test-utils";
+import { expect, assert } from "chai";
+import sinon from "sinon";
+import auth0 from "auth0-js";
+import Header from "../src/scripts/components/Header";
+import resoundAPI from "../src/scripts/services/resound-api";
 
-
-describe('<Header />', function() {
+describe("<Header />", function() {
   beforeEach(() => {
     const history = { history: [] };
-    this.auth0Stub = sinon.stub(auth0.WebAuth.prototype, 'authorize');
+    this.auth0Stub = sinon.stub(auth0.WebAuth.prototype, "authorize");
     this.component = TestUtils.renderIntoDocument(
       <MemoryRouter>
         <Header auth={resoundAPI.auth} history={history} />
@@ -26,16 +25,22 @@ describe('<Header />', function() {
     this.auth0Stub.restore();
   });
 
-  it('shows log in button', () => {
-    const loginButton = TestUtils.findRenderedDOMComponentWithClass(this.component, 'header__button');
+  it("shows log in button", () => {
+    const loginButton = TestUtils.findRenderedDOMComponentWithClass(
+      this.component,
+      "header__button"
+    );
     expect(loginButton).to.exist;
-    expect(loginButton.innerHTML).to.equal('log in');
+    expect(loginButton.innerHTML).to.equal("log in");
   });
 
-  it('clicking login will try to authorize the user', () => {
-    const loginButton = TestUtils.findRenderedDOMComponentWithClass(this.component, 'header__button');
+  it("clicking login will try to authorize the user", () => {
+    const loginButton = TestUtils.findRenderedDOMComponentWithClass(
+      this.component,
+      "header__button"
+    );
     TestUtils.Simulate.click(loginButton);
 
-    assert(this.auth0Stub.called, 'auth0 has been called');
+    assert(this.auth0Stub.called, "auth0 has been called");
   });
 });
