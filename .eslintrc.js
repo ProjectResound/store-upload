4679211b8c2bc86132b752958f609aa15669844c
Louise Yang <louise.m.yang@gmail.com>
2017-08-28 12:09:49 -0700

    Adds more tests to Audio page and specs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,28 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
+  parser: "babel-eslint",
   parserOptions: {
-    sourceType: 'module',
+    sourceType: "module",
     ecmaFeatures: {
       jsx: true
     },
     ecmaVersion: 6
   },
-  extends: [
-    'prettier',
-    'prettier/react'
-  ],
-  plugins: [
-    'import',
-    'prettier'
-  ],
-  'rules': {
-    "prettier/prettier": [
-      "error",
-      {"singleQuote": false},
-    ],
+  extends: ["prettier", "prettier/react"],
+  plugins: ["import", "prettier"],
+  rules: {
+    "prettier/prettier": ["error", { singleQuote: false }],
     "jsx-a11y/img-has-alt": 0,
     "jsx-a11y/no-static-element-interactions": 0,
     "react/jsx-no-bind": 0
   },
   globals: {
-    "fetch": true,
-    "window": true,
-    "describe": true,
-    "beforeEach": true,
-    "afterEach": true,
-    "it": true,
-    "localStorage": true
+    fetch: true,
+    window: true,
+    describe: true,
+    beforeEach: true,
+    afterEach: true,
+    it: true,
+    localStorage: true
   }
 };
--- test/contributor.spec.js
+++ test/contributor.spec.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils from 'react-dom/test-utils'
-import sinon from 'sinon';
-import Contributor from '../src/scripts/components/contributor/Contributor';
-import {expect, assert} from 'chai';
+import React from "react";
+import ReactDOM from "react-dom";
+import TestUtils from "react-dom/test-utils";
+import sinon from "sinon";
+import Contributor from "../src/scripts/components/contributor/Contributor";
+import { expect, assert } from "chai";
 
-describe('<Contributor />', function() {
+describe("<Contributor />", function() {
   beforeEach(() => {
     this.component = TestUtils.renderIntoDocument(<Contributor />);
     this.componentDOM = ReactDOM.findDOMNode(this.component);
@@ -15,22 +15,29 @@ describe('<Contributor />', function() {
     ReactDOM.unmountComponentAtNode(this.componentDOM.parentNode);
   });
 
-  it('renders a Contributor', () => {
-    return expect(this.component, 'to have rendered', <Contributor />);
+  it("renders a Contributor", () => {
+    return expect(this.component, "to have rendered", <Contributor />);
   });
 
-  it('when input is comma separated, show correct suggestions', () => {
-    const suggestions = ['katie', 'louise', 'josie'];
+  it("when input is comma separated, show correct suggestions", () => {
+    const suggestions = ["katie", "louise", "josie"];
     const stubbedFunc = () => {};
-    this.component = TestUtils.renderIntoDocument(<Contributor contributorsSuggestions={suggestions}
-                                                               value=''
-                                                               onChangeContributor={stubbedFunc} />);
+    this.component = TestUtils.renderIntoDocument(
+      <Contributor
+        contributorsSuggestions={suggestions}
+        value=""
+        onChangeContributor={stubbedFunc}
+      />
+    );
     this.componentDOM = ReactDOM.findDOMNode(this.component);
-    const input = TestUtils.findRenderedDOMComponentWithTag(this.component, 'input');
-    const spy = sinon.spy(this.component, 'getSuggestions');
+    const input = TestUtils.findRenderedDOMComponentWithTag(
+      this.component,
+      "input"
+    );
+    const spy = sinon.spy(this.component, "getSuggestions");
     TestUtils.Simulate.focus(input);
-    input.value = 'alex, k';
+    input.value = "alex, k";
     TestUtils.Simulate.change(input);
-    assert(spy.returned(['katie']), 'returns correct suggestion');
+    assert(spy.returned(["katie"]), "returns correct suggestion");
   });
 });
