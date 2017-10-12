826a6b34fff3fd82e2ff5106f4333bdc3fb34680
Louise Yang <louise.m.yang@gmail.com>
2017-10-12 15:12:54 -0700

    Makes audio URL easier to read

--- src/scripts/services/audio-tools.js
+++ src/scripts/services/audio-tools.js
@@ -5,7 +5,10 @@ exports.getCreatedAt = audio => new Date(audio.created_at).toLocaleDateString();
 exports.getDuration = audio =>
   moment.utc(audio.duration * 1000).format("HH:mm:ss");
 
-exports.generateUrl = audio => `/audio/${audio.id}-${encodeURI(audio.title)}`;
+exports.generateUrl = audio => {
+  const title = audio.title.replace(/\s+/g, "_");
+  return `/audio/${audio.id}-${encodeURI(title)}`;
+};
 
 exports.isValidLength = (string, maxLength) =>
   string.length > 0 && string.length >= maxLength;
