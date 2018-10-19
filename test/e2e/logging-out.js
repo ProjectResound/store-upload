45f97f53eef46179744fa1add81237021c340cef
Joann Im <joann_im@hotmail.com>
2018-10-19 10:46:38 -0700

    Create test for logging out

---
+++ test/e2e/logging-out.js
@@ -0,0 +1,22 @@
+import { Selector, Role } from 'testcafe';
+
+
+const indexPage = "http://localhost:8000";
+
+const louise = Role(indexPage, async t => {
+  await t
+    .typeText('[name="email"]', process.env.TESTCAFE_EMAIL)
+    .typeText('[name="password"]', process.env.TESTCAFE_PW)
+    .click('[type="submit"]');
+});
+
+fixture("Logged In")
+  .page(indexPage);
+
+test("Logging out", async t => {
+  await t
+    .useRole(louise)
+    .navigateTo(indexPage)
+    .click(".header__button")
+    .expect(Selector(".auth0-lock-input").exists).ok()
+});
