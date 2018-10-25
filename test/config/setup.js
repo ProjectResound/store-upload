2c7dded8d64e1d3b0471d9334bf9b9b8c77a0240
Louise Yang <louise.m.yang@gmail.com>
2018-10-25 11:43:51 -0700

    Linter cleanup

--- test/config/setup.js
+++ test/config/setup.js
@@ -1,8 +1,8 @@
 require("jsdom-global")();
 var Dotenv = require("dotenv-webpack");
-var envEntries = (new Dotenv({ path: "./test/config/.env.test" })).definitions;
+var envEntries = new Dotenv({ path: "./test/config/.env.test" }).definitions;
 var envProps = Object.keys(envEntries).reduce(function(acc, prop) {
-  var propName = (prop.split('.'))[2];
+  var propName = prop.split(".")[2];
   var propValue = envEntries[prop].replace(/\"/g, "");
   acc[propName] = propValue;
 
@@ -10,4 +10,3 @@ var envProps = Object.keys(envEntries).reduce(function(acc, prop) {
 }, {});
 console.log(envProps);
 global.process.env = envProps;
-
