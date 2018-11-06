adc98cf0b07c12e0553b8c112bf9865c7a94acb6
Jônatas Rancan de Souza <jonatasrancan@users.noreply.github.com>
2018-11-06 16:34:10 -0200

    Add express-sslify to force https requests (#33)
    Uses TLS in heroku environment

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+const enforce = require('express-sslify');
 const port = process.env.PORT || 8000;
 const app = express();
+
+// Force https requests
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
  // the __dirname is the current directory from where the script is running
 // app.use(express.static(__dirname));
 app.use(express.static(__dirname + '/dist'));
