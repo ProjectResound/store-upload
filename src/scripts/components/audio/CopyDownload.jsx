c15a13af77d1af2486ccf9dfbb8dafdb4039e1a7
Louise Yang <louise.m.yang@gmail.com>
2018-12-20 11:33:25 -0800

    Fix postToCMS to allow for https

--- src/scripts/components/audio/CopyDownload.jsx
+++ src/scripts/components/audio/CopyDownload.jsx
@@ -15,10 +15,6 @@ export default class CopyDownload extends React.Component {
   postUrlToOpener(url) {
     if (!window.opener || !CONSTANTS.cmsUrl) return;
     window.opener.postMessage({ type: "url", value: url }, CONSTANTS.cmsUrl);
-    window.opener.postMessage(
-      { type: "url", value: url },
-      CONSTANTS.cmsUrl.replace(/https:/i, "http:")
-    );
     this.setState({ sentToCms: true });
   }
 
