5ee995ee8ea320737337339a097e3526bb0c34c7
Joann Im <joann_im@hotmail.com>
2019-03-04 13:49:23 -0800

    Add loading message when audio or waveform is loading

--- src/scripts/components/embed/Embed.jsx
+++ src/scripts/components/embed/Embed.jsx
@@ -4,6 +4,7 @@ import { getDuration } from "../../services/audio-tools";
 import addFallbackIfNecessary from "../../services/audio-context";
 import autoBind from "react-autobind";
 import Wavesurfer from "react-wavesurfer";
+import wavesurfer from "wavesurfer.js";
 import qs from "qs";
 
 class Embed extends Component {
@@ -13,8 +14,10 @@ class Embed extends Component {
     this.state = {
       addFallbackAudioElement: false,
       audio: {},
+      audioState: "loading",
       currentTime: "00:00",
-      playing: false
+      playing: false,
+      waveState: "loading"
     };
 
     autoBind(this);
@@ -27,6 +30,13 @@ class Embed extends Component {
 
     // Checks if browser has AudioContext and if not add HTML5 audio element as fallback
     addFallbackIfNecessary(this);
+    this.checkWaveformState();
+  }
+
+  checkWaveformState() {
+    wavesurfer.on("waveform-ready", () => {
+      this.setState({ waveState: "ready" });
+    });
   }
 
   handlePosChange(e) {
@@ -46,12 +56,13 @@ class Embed extends Component {
     const durationInSeconds = e.wavesurfer.backend.media.duration;
 
     this.setState({
+      audioState: "ready",
       duration: getDuration({ duration: durationInSeconds })
     });
   }
 
   render() {
-    const { audio, currentTime, duration } = this.state;
+    const { audio, audioState, currentTime, duration, waveState } = this.state;
 
     const waveSurferOptions = {
       backend: "MediaElement",
@@ -59,7 +70,9 @@ class Embed extends Component {
       cursorWidth: 0,
       height: 75,
       normalize: true,
-      progressColor: audio.progressColor ? audio.progressColor : "rgb(41, 213, 239)",
+      progressColor: audio.progressColor
+        ? audio.progressColor
+        : "rgb(41, 213, 239)",
       responsive: true,
       waveColor: audio.waveColor ? audio.waveColor : "rgba(0, 0, 0, 0.1)"
     };
@@ -102,20 +115,37 @@ class Embed extends Component {
                     </div>
                   </div>
                 </div>
-                <Wavesurfer
-                  audioFile={`http://localhost:3000/${audio.url}`}
-                  className="embed__waveform"
-                  onPosChange={this.handlePosChange}
-                  onReady={this.setDuration}
-                  options={waveSurferOptions}
-                  playing={this.state.playing}
-                />
-                {currentTime &&
-                  duration && (
-                    <div className="embed__timestamp">
-                      {this.state.currentTime} / {this.state.duration}
+                <div className="embed__waveform-container">
+                  {audioState === "loading" && (
+                    <div className="embed__loading-msg pulsate">
+                      <span>loading audio...</span>
                     </div>
                   )}
+                  {audioState === "ready" &&
+                    waveState === "loading" && (
+                      <div className="embed__loading-msg pulsate">
+                        <span>loading waveform...</span>
+                      </div>
+                    )}
+                  <Wavesurfer
+                    audioFile={`http://localhost:3000/${audio.url}`}
+                    className={`embed__waveform ${
+                      audioState === "loading" || waveState === "loading"
+                        ? "hide"
+                        : ""
+                    }`}
+                    onPosChange={this.handlePosChange}
+                    onReady={this.setDuration}
+                    options={waveSurferOptions}
+                    playing={this.state.playing}
+                  />
+                  {currentTime &&
+                    duration && (
+                      <div className="embed__timestamp">
+                        {this.state.currentTime} / {this.state.duration}
+                      </div>
+                    )}
+                </div>
               </div>
             </div>
           )}
