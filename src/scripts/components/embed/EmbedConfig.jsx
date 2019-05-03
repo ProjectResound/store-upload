0c47ba6546ab53072eb673d2bfcfe7f20e5f4e4f
Joann Im <joann_im@hotmail.com>
2019-05-03 13:42:11 -0700

    Fix bug where pasting a hex code to the input doesn't change the player's color

--- src/scripts/components/embed/EmbedConfig.jsx
+++ src/scripts/components/embed/EmbedConfig.jsx
@@ -32,22 +32,42 @@ class EmbedConfig extends Component {
     const state = {};
 
     if (element === "player") {
-      const o = Math.round(
-        (parseInt(r) * 299 + parseInt(g) * 587 + parseInt(b) * 114) / 1000
-      );
+      if (r !== undefined && g !== undefined && b !== undefined) {
+        const o = Math.round(
+          (parseInt(r) * 299 + parseInt(g) * 587 + parseInt(b) * 114) / 1000
+        );
 
-      // If the background color is bright change the text color to black
-      if (o > 125) {
-        state.textColor = "black";
-      } else {
-        // If background color is dark change the text color to white
-        state.textColor = "white";
+        // If the background color is bright change the text color to black
+        if (o > 125) {
+          state.textColor = "black";
+        } else {
+          // If background color is dark change the text color to white
+          state.textColor = "white";
+        }
       }
     }
 
     state[`${element}Color`] = hex;
 
     this.setState(state);
+  }
+
+  hexToRgb(hex) {
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+      return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+    return result
+      ? {
+          r: parseInt(result[1], 16),
+          g: parseInt(result[2], 16),
+          b: parseInt(result[3], 16)
+        }
+      : null;
   }
 
   toggleColorPicker(element) {
@@ -104,7 +124,8 @@ class EmbedConfig extends Component {
     const validHexCode = regExp.test(hex);
 
     if (validHexCode) {
-      this.changeColor(element, { hex });
+      const rgb = this.hexToRgb(hex);
+      this.changeColor(element, { hex, rgb });
     }
   }
 
